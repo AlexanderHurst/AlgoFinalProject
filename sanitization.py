48245595374102683c2512715f9865ec048d35e4
Alexander Hurst <alexander@ahmail.ca>
2018-11-20 15:50:20 -0500

    added comments

--- sanitization.py
+++ sanitization.py
@@ -1,8 +1,12 @@
 import re
 
 
+# removes all characters that are not part of legal characters regex
+# replaces them with replacement "" for nothing
 def sanitize(string, legal_characters, replacement):
 
+    # create the regex
+    # & perform the replacement
     regex = re.compile(legal_characters)
     string = regex.sub(replacement, string).upper()
     return string
