81a12d42fb4cf82fd99bde2a4a6d0b92bced6dbb
Alexander Hurst <alexander@ahmail.ca>
2018-11-21 18:08:52 -0500

    changed debugging to main -> tester function

--- suffix_trie.py
+++ suffix_trie.py
@@ -60,41 +60,42 @@ class trie_node:
         return string
 
 
-# this is going to be fun
+if __name__ == "__main__":
+    # this is going to be fun
 
-suffix_trie2 = trie_node('d')
-suffix_trie2.add_child('b')
-suffix_trie2.add_child('c')
-suffix_trie2.add_child('d')
-suffix_trie2.add_child('e')
+    suffix_trie2 = trie_node('d')
+    suffix_trie2.add_child('b')
+    suffix_trie2.add_child('c')
+    suffix_trie2.add_child('d')
+    suffix_trie2.add_child('e')
 
-suffix_trie = trie_node('c')
-suffix_trie.add_child('b')
-suffix_trie.add_child('c')
-suffix_trie.add_child('d')
-suffix_trie.add_child('e')
-suffix_trie.add_child(suffix_trie2)
+    suffix_trie = trie_node('c')
+    suffix_trie.add_child('b')
+    suffix_trie.add_child('c')
+    suffix_trie.add_child('d')
+    suffix_trie.add_child('e')
+    suffix_trie.add_child(suffix_trie2)
 
-temp_suffix_trie = trie_node('g')
-temp_suffix_trie.add_child('f')
-temp_suffix_trie.add_child('h')
-temp_suffix_trie.add_child('j')
-temp_suffix_trie.add_child('k')
+    temp_suffix_trie = trie_node('g')
+    temp_suffix_trie.add_child('f')
+    temp_suffix_trie.add_child('h')
+    temp_suffix_trie.add_child('j')
+    temp_suffix_trie.add_child('k')
 
-temp2_suffix_trie = trie_node('r')
-temp2_suffix_trie.add_child('f')
-temp2_suffix_trie.add_child('h')
-temp2_suffix_trie.add_child('j')
-temp2_suffix_trie.add_child('k')
+    temp2_suffix_trie = trie_node('r')
+    temp2_suffix_trie.add_child('f')
+    temp2_suffix_trie.add_child('h')
+    temp2_suffix_trie.add_child('j')
+    temp2_suffix_trie.add_child('k')
 
-for i in suffix_trie.get_each():
-    if i.get_character() == 'e':
-        i.add_child(temp_suffix_trie)
+    for i in suffix_trie.get_each():
+        if i.get_character() == 'e':
+            i.add_child(temp_suffix_trie)
 
-add_list = []
-for i in suffix_trie.get_each():
-    if i.get_character() == 'k':
-        add_list.append(i)
-for i in add_list:
-    i.add_child(temp2_suffix_trie)
-print(suffix_trie)
+    add_list = []
+    for i in suffix_trie.get_each():
+        if i.get_character() == 'k':
+            add_list.append(i)
+    for i in add_list:
+        i.add_child(temp2_suffix_trie)
+    print(suffix_trie)
